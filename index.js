11ff730f22e3030061073116a7ea713f6f8b7998
Joseph <jteklu@outlook.com>
2023-05-08 08:58:08 -0600

    inital commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const inquirer = require("inquirer");
+const mysql = require('mysql2');
+const consoleTable = require('console.table')
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // MySQL password
+    password: '',
+    database: 'classlist_db'
+  },
+  console.log(`Connected to the classlist_db database.`)
+);
