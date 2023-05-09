5b99d9c12ff3118b5159ae4356b60b9668567d86
Joseph Teklu <jteklu@outlook.com>
2023-05-09 14:28:40 -0600

    recurscion on inquirer questions and set up tables with seeds for db

---
+++ db/schema.sql
@@ -0,0 +1,39 @@
+DROP DATABASE IF EXISTS Employee_Tracker;
+CREATE DATABASE Employee_Tracker;
+
+USE Employee_Tracker;
+
+CREATE TABLE department (
+    id INT NOT NULL,
+    name VARCHAR(30),
+    -- make the id a primary key for the role table
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role(
+    id INT NOT NULL,
+    PRIMARY KEY (id), -- make id a primary key for the employee table
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT, -- refrence to department role 
+
+    -- specify that the (department_id) will come from a different table
+    FOREIGN KEY(department_id)
+    -- refrence the id property from the department table for the foriegn key
+    REFERENCES department(id)
+    -- if a department gets deletes what ever rol is using it will be set it to null
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee(
+    id INT NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT NOT NULL,
+    manager_id INT -- null if employee has no manager
+
+    -- specify that the (id) will come from a different table
+    FOREIGN KEY(id)
+    -- refrence the id from the table role to use for the foriegn key
+    REFERENCES role(id)
+);
--- index.js
+++ index.js
@@ -1,20 +1,56 @@
-const inquirer = require("inquirer");
+const express = require('express');
+const inquirer = require('inquirer'); 
 const mysql = require('mysql2');
-const consoleTable = require('console.table')
+
+const PORT = process.env.PORT || 3001;
+const app = express();
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// array for questions
+const questions = 
+[
+  {type:"list",name:"choice", 
+  choices:["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an eployee", "Update an employee role", "Exit"],
+  default: "View all departments"
+  }
+]
 
 // Connect to database
 const db = mysql.createConnection(
   {
     host: 'localhost',
     // MySQL username,
-    user: 'root',
+    user: 'joe',
     // MySQL password
     password: '',
     database: 'classlist_db'
   },
   console.log(`Connected to the classlist_db database.`)
 );
+
+function recur(){
+  // send inquirer the questions array
+  inquirer.prompt(questions)
+  .then(response =>{
+    // if the choosen slection is exit then return false
+    if(response.choice === "Exit") {return 0;}
+    // if the choosen selection is not exit then return the user's choice
+    else{return response.choice;}
+  })
+  .then(res => {
+    // if the response is false return
+    if(!res) {return;}
+    // or else call this function agian to display the options from inquirer
+    else yellow();
+  })
+}
+
+function init() {
+  // call recur and get use's choice
+  recur();
+}
+
+init();
